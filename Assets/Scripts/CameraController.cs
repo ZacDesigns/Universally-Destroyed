2c55de7b545d99acd1995e0854afa5e6a5481cb4
Zac <zacmaibusiness@gmail.com>
2021-08-14 17:35:52 +1000

    Basic Character Controller and Camera setup

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float mouseSenitivity;
+
+    void Update()
+    {
+        //Mouse Input
+        var h = Input.GetAxisRaw("Mouse X") * mouseSenitivity;
+        var v = Input.GetAxisRaw("Mouse Y") * mouseSenitivity;
+
+        //Camera Vertical rotation
+        Vector3 look = new Vector3(-v, 0, 0);
+        Vector3 cameraRotation = transform.rotation.eulerAngles;
+        transform.localRotation = Quaternion.Euler(cameraRotation.x + look.x,0,0);
+
+
+        //Camera Horizontal rotation
+        Vector3 playerTransfrom = transform.parent.rotation.eulerAngles;
+        Vector3 playerLook = new Vector3(0, h, 0);
+        transform.parent.rotation = Quaternion.Euler(playerTransfrom + playerLook);
+
+    }
+}
---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public float walkSpeed;
+    public float sprintSpeed;
+    public float jumpHeight;
+
+    [Space]
+    public Rigidbody rb;
+    public Transform groundChecker;
+    public LayerMask ground;
+    Vector3 movement;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        movement = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
+        RaycastHit hit;
+        if (Physics.Raycast(groundChecker.position, Vector3.down, out hit, .1f, ground))
+        {
+
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(new Vector3(0, jumpHeight, 0), ForceMode.Impulse);
+            }
+
+        }
+    }
+
+    void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+
+    void moveCharacter(Vector3 direction)
+    {
+
+
+        direction = rb.rotation * direction;
+
+       
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            rb.MovePosition(rb.position + direction * sprintSpeed * Time.fixedDeltaTime);
+        }
+        else
+        {
+            rb.MovePosition(rb.position + direction * walkSpeed * Time.fixedDeltaTime);
+        }
+    }
+
+ 
+}
