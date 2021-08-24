c1b59a0624568100abf0fa76200f1e3694ed03c7
Zac <zacmaibusiness@gmail.com>
2021-08-24 12:34:06 +1000

    New ending scene creation and script changes

--- Assets/Scripts/EndingTrigger.cs
+++ Assets/Scripts/EndingTrigger.cs
@@ -1,18 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndingTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void endState()
     {
-        
+        SceneManager.LoadScene(2);
+        Time.timeScale = 1f;
+        PauseMenuController.isPaused = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
---
+++ Assets/Scripts/LeavingEndscreen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LeavingEndscreen : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
